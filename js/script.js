d23fc14893ecc4df060e6c6a60fdb71541529ea0
Joao-Bonifacio <joaojunior.b22@gmail.com>
2022-02-01 20:41:29 -0300

    chart1.1

--- js/script.js
+++ js/script.js
@@ -5,6 +5,7 @@ var date = document.getElementById('date')
 var val = document.getElementById('val')
 var radio = document.querySelectorAll('.radio')
 var feetChart = document.querySelector('#feetChart')
+var arrow = document.getElementById('arrow')
 arrField = localStorage.getItem('x')
 arrData = localStorage.getItem('y')
 arrVal = localStorage.getItem('val')
@@ -19,32 +20,6 @@ if (localStorage.x == undefined){
 }else{
     arrField = localStorage.x.split(',')
     arrData = localStorage.y.split(',')
-}
-if (localStorage.val != undefined) {
-    arrVal = localStorage.val.split(',')
-    for (let i = 1; i < arrVal.length; i++) {
-        //adicionando os valores de localStorage.val em uma div filha
-        let divFactory = document.createElement('div')
-        let pFactory = document.createElement('p')
-        let content = document.createTextNode(arrVal[i])
-        let spanFactory = document.createElement('span')
-        let p2 = document.createElement('p')
-        let iFactoryPen = document.createElement('i')
-        let iFactoryTrash = document.createElement('i') 
-
-        iFactoryPen.setAttribute('class','fas fa-pen')
-        iFactoryTrash.setAttribute('class','fas fa-trash')
-        spanFactory.appendChild(iFactoryPen)
-        spanFactory.appendChild(iFactoryTrash)
-        p2.appendChild(spanFactory)
-        divFactory.classList.add('feet_child')
-        pFactory.appendChild(content)
-        divFactory.appendChild(pFactory)
-        divFactory.appendChild(p2)
-        feetChart.appendChild(divFactory)
-    }//i = 1 para não pegar o primeiro valor de localStorage.val que é null
-}else{
-    localStorage.setItem('val',arrVal)
 }
 
 const setChart = (x,y)=>{
@@ -79,6 +54,40 @@ const setChart = (x,y)=>{
     }
 }
 
+const setFeetChart = (t)=>{
+    //adicionar os valores ao clicar na seta para baixo
+    if (t == 'down') {
+        if (localStorage.val != undefined) {
+            arrVal = localStorage.val.split(',')
+            for (let i = 1; i < arrVal.length; i++) {
+                //adicionando os valores de localStorage.val em uma div filha
+                let divFactory = document.createElement('div')
+                let pFactory = document.createElement('p')
+                let content = document.createTextNode(arrVal[i])
+                let spanFactory = document.createElement('span')
+                let p2 = document.createElement('p')
+                let iFactoryPen = document.createElement('i')
+                let iFactoryTrash = document.createElement('i') 
+        
+                iFactoryPen.setAttribute('class','fas fa-pen clk')
+                iFactoryTrash.setAttribute('class','fas fa-trash clk')
+                spanFactory.appendChild(iFactoryPen)
+                spanFactory.appendChild(iFactoryTrash)
+                p2.appendChild(spanFactory)
+                divFactory.classList.add('feet_child')
+                pFactory.appendChild(content)
+                divFactory.appendChild(pFactory)
+                divFactory.appendChild(p2)
+                feetChart.appendChild(divFactory)
+            }//i = 1 para não pegar o primeiro valor de localStorage.val que é null
+        }else{
+            localStorage.setItem('val',arrVal)
+        }
+    }else if (t == 'up') {
+        feetChart.innerHTML = ""
+    }
+}
+
 document.querySelector('#send').addEventListener('click',()=>{
     if (date.value != null && val.value != null) {       
         setChart(date.value,val.value)
@@ -86,7 +95,21 @@ document.querySelector('#send').addEventListener('click',()=>{
         alert('Erro: Passe todos os valores do formulário')
     }
 })
-
+arrow.addEventListener('click',()=>{
+    if (arrow.classList.contains('fa-arrow-down')) {
+        setFeetChart('down')
+        arrow.style = 'margin-top: -25px;border-radius: 0 0 10px 10px;text-align: center;'
+        arrow.removeAttribute('class')
+        arrow.setAttribute('class','fas fa-arrow-up')
+        arrow.parentElement.parentElement.setAttribute('title','mostrar menos')
+    }else{
+        setFeetChart('up')
+        arrow.style = 'margin-top: -25px;border-radius: 0 0 10px 10px;text-align: center;'
+        arrow.removeAttribute('class')
+        arrow.setAttribute('class','fas fa-arrow-down')
+        arrow.parentElement.parentElement.setAttribute('title','mostrar mais')
+    }
+})
 const getLabels = ()=>{
     return arrField
 }
